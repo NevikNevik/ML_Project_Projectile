81fb8ecdf79e91bcc304b9c983496b776a75cfd3
NevikNevik <schrimlkevin@gmail.com>
2025-04-21 18:36:19 -0400

    Updated code with corrected input data locations and metrics

--- Neural_network_FEA.py
+++ Neural_network_FEA.py
@@ -13,13 +13,14 @@ from matplotlib import pyplot as plt
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-
+from sklearn.metrics import r2_score, mean_squared_error
+import numpy as np
 
 #importing training dataset and target files
-dataset = pd.read_csv('Finite_element_dataset.csv')
-X = dataset.iloc[:, :].values
-dataset2 = pd.read_csv('Finite_element_target.csv')
-Y = dataset2.iloc[:, :].values
+dataset = pd.read_csv('Finite_element_entire_data_set.csv')
+X = dataset.iloc[:, 0:5]
+Y = dataset.iloc[:, 5].values
+
 
 #Column Transfer and one hot encoding for categorical features
 ct=ColumnTransformer(transformers=[("oh",OneHotEncoder(),[2,3,4])],remainder="passthrough")
@@ -42,8 +43,12 @@ sns.kdeplot(scaled[10],ax=ob2)
 sns.kdeplot(scaled[11],ax=ob2)
 
 #Splitting the training data from the unseen inputs for which predictions are to be made
-Train= Z.iloc[0:3221]
-Input= Z.iloc[3221::]
+print(Z.shape)
+Train = Z.iloc[:3041]      # 0 to 3039 → total 3040 rows
+Y_train = Y[:3041]         # match the same rows in target
+Input = Z.tail(1)          # safe way to grab the last row (3040)
+
+
 
 #Splitting the data into training, validation and testing datasets
 train_x, test_x, train_y, test_y = train_test_split(Train, Y, test_size=0.20, random_state=415)
@@ -61,10 +66,11 @@ model.add(Dense(1,))
 model.compile( loss='mse', optimizer='adam')
 
 #Compiling the model with early stopping algorithm
-monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=10, verbose=1, mode='auto')
-
+#monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=10, verbose=1, mode='auto')
 #Computing training and validation loss for the network
-history= model.fit(train_x,train_y,validation_data=(validation_x,validation_y),callbacks=[monitor],verbose=2,epochs=1000)
+#history= model.fit(train_x,train_y,validation_data=(validation_x,validation_y),callbacks=[monitor],verbose=2,epochs=1000)
+history= model.fit(train_x,train_y,validation_data=(validation_x,validation_y),verbose=2,epochs=500)
+
 history_dict=history.history
 loss_values= history_dict['loss']
 val_loss_values=history_dict['val_loss']
@@ -93,6 +99,14 @@ def average(Error_testing):
     return sum(abs(Error_testing))/len(Error_testing)
 
 Testing_Relative_Error= average(Error_testing)
+
+
+
+mae_val = mean_squared_error(validation_y, y_validation_pred)
+r2_val = r2_score(validation_y, y_validation_pred)
+
+print("Validation MSE:", mae_val)
+print("Validation R²:", r2_val)
 
 
 
