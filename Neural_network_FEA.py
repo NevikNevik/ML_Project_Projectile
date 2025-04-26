73673f44fbb292b03a473aa8e252d95c6aeb8e45
NevikNevik <schrimlkevin@gmail.com>
2025-04-26 08:38:11 -0400

    project files

--- Neural_network_FEA.py
+++ Neural_network_FEA.py
@@ -14,6 +14,7 @@ from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.tree import DecisionTreeRegressor
 import numpy as np
 
 #importing training dataset and target files
@@ -54,63 +55,22 @@ Input = Z.tail(1)          # safe way to grab the last row (3040)
 train_x, test_x, train_y, test_y = train_test_split(Train, Y, test_size=0.20, random_state=415)
 validation_x, testing_x, validation_y, testing_y = train_test_split(test_x,test_y,test_size=0.50, random_state=415)
 
-#importing and implementing Neural network regression model
-model = Sequential()
-model.add(Dense(12, activation='relu', input_shape=(12,)))
-model.add(Dense(30, activation='relu'))
-model.add(Dense(30, activation='relu'))
-model.add(Dense(30, activation='relu'))
+model = DecisionTreeRegressor(criterion='squared_error',
+splitter='best', max_depth=None, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0,
+random_state=42)
 
 
-model.add(Dense(1,))
-model.compile( loss='mse', optimizer='adam')
+model.fit(train_x, train_y)
 
-#Compiling the model with early stopping algorithm
-#monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=10, verbose=1, mode='auto')
-#Computing training and validation loss for the network
-#history= model.fit(train_x,train_y,validation_data=(validation_x,validation_y),callbacks=[monitor],verbose=2,epochs=1000)
-history= model.fit(train_x,train_y,validation_data=(validation_x,validation_y),verbose=2,epochs=500)
+#Predict and evaluate
+preds = model.predict(validation_x)
+mae = mean_squared_error(validation_y, preds)
+r2 = r2_score(validation_y, preds)
 
-history_dict=history.history
-loss_values= history_dict['loss']
-val_loss_values=history_dict['val_loss']
-
-#Getting predictions for training dataset
-y_train_pred =model.predict(train_x)
-#Getting predictions for validation dataset
-y_validation_pred= model.predict(validation_x)
-#Getting predictions for testing dataset
-final_result= model.predict(testing_x)
-#Getting predictions for unseen inputs
-Unseen_result = model.predict(Input)
-
-#Calculating relative error for validation dataset
-Error= ((validation_y-y_validation_pred)/validation_y)
-
-def average(Error):
-    return sum(abs(Error))/len(Error)
-
-Relative_Error= average(Error)
-
-#Calculating relative error for testing dataset
-Error_testing= ((testing_y-final_result)/testing_y)
-
-def average(Error_testing):
-    return sum(abs(Error_testing))/len(Error_testing)
-
-Testing_Relative_Error= average(Error_testing)
-
-
-
-mae_val = mean_squared_error(validation_y, y_validation_pred)
-r2_val = r2_score(validation_y, y_validation_pred)
-
-print("Validation MSE:", mae_val)
-print("Validation R²:", r2_val)
-
-
-
-
+print(f'Mean Absolute Error: {mae:.2f}')
+#print(validation_y)
+#print(preds)
+print(f'r_squared: {r2:.2f}')
 
 
 
